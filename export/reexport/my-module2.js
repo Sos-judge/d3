658d2b7dd5b2231b520f8122276cc2ccebf170d2
Sos-judge <59525015+Sos-judge@users.noreply.github.com>
2021-10-22 15:39:44 +0800

    Create my-module2.js

---
+++ export/reexport/my-module2.js
@@ -0,0 +1,11 @@
+var graph = {
+    options: {
+        color:'white',
+        thickness:'2px'
+    },
+    draw: function() {
+        console.log('From graph draw function');
+    }
+}
+
+export {graph};
